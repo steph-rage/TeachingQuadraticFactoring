52f6ba966d02b26f7016b2567a73c3b0a5175693
steph-rage <stephlage621@gmail.com>
2017-01-20 18:10:32 -0500

    Starting to add factoring numbers practice

--- Teaching.py
+++ Teaching.py
@@ -1,15 +1,22 @@
+#!user/bin/python
+
 from flask import Flask, render_template
+from random import randint
+
 factorteacher = Flask(__name__)
 
+
 @factorteacher.route('/factorteacher/')
-def load_start(name=None):
-    return render_template('StartPage.html')
+def load_start():
+	return render_template('StartPage.html')
 
 @factorteacher.route('/factorteacher/what/')
-def load_what(name=None):
+def load_what():
     return render_template('HowtoWhataWhat.html')
 
 @factorteacher.route('/factorteacher/what/factornumbers')
-def load_numbers_examples(name=None):
-    return render_template('FactorNumbersExamples.html')
+def load_numbers_examples():
+	number_to_factor = 8
+	number_of_pairs = 2
+	return render_template('FactorNumbers.html', number_to_factor=number_to_factor, number_of_pairs=number_of_pairs)
 
